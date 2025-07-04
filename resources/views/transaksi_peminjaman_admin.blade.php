1dcd8867ef2ff11feee8496a98cef677d708a68d
Albertus Surya Pangestu <albertussuryapangestu@gmail.com>
2025-07-04 20:46:58 +0700

    Selesaikan konflik merge dari develop

--- resources/views/transaksi_peminjaman_admin.blade.php
+++ resources/views/transaksi_peminjaman_admin.blade.php
@@ -103,10 +103,7 @@
                             <tr class="border-b">
                                 <th class="py-3 px-4 text-left">No</th>
                                 <th class="py-3 px-4 text-left">Tanggal Pinjam</th>
-<<<<<<< HEAD
-=======
                                 <th class="py-3 px-4 text-left">Tanggal Kembali</th>
->>>>>>> develop
                                 <th class="py-3 px-4 text-left">Nama Peminjam</th>
                                 <th class="py-3 px-4 text-left">Nama Barang</th>
                                 <th class="py-3 px-4 text-left">Jumlah</th>
@@ -119,13 +116,9 @@
                             <tr class="border-b">
                                 <td class="py-3 px-4">{{ $loop->iteration }}</td>
                                 <td class="py-3 px-4">
-<<<<<<< HEAD
-                                    {{ $item->tanggal_pinjam }}<br>
-=======
                                     {{ $item->tanggal_pinjam }}
                                 </td>
                                 <td class="py-3 px-4">
->>>>>>> develop
                                     {{ $item->tanggal_kembali }}
                                 </td>
                                 <td class="py-3 px-4">{{ $item->nama_peminjam }}</td>
